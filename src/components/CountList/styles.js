5aec75bba1c106321d62374c320ea704f7630e9e
Joe Cocco <cocco3@gmail.com>
2019-04-08 00:00:27 -0500

    gh pages

--- src/components/CountList/styles.js
+++
@@ -1,7 +0,0 @@
-export default {
-  CountList: {
-    margin: '1em 0',
-    maxWidth: '800px',
-    padding: '0 24px'
-  }
-}
--- src/utilities/index.js
+++
@@ -1,2 +0,0 @@
-export { default as GoogleApi } from './GoogleApi'
-export { default as Initials } from './Initials'
